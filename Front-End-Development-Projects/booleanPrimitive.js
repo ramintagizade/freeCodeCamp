a219a38d247d7e892dd58f83785d625be2c461de
Ramin Taghizada <ramintagizade@mail.ru>
2017-05-20 20:49:30 +0200

    moved

--- Front-End-Development-Projects/booleanPrimitive.js
+++
@@ -1,8 +0,0 @@
-
-function booWho(variable) {
-  // What is the new fad diet for ghost developers? The Boolean.
-  if(typeof(variable) === "boolean") return true;
-  return false;
-}
-
-booWho(null);
